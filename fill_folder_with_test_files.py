7cc3e1e516e05b1d615a8f864f073a1aebb80e03
Dikiblood <dikiblood@bk.ru>
2021-02-16 20:23:31 +0300

    new project

---
+++ fill_folder_with_test_files.py
@@ -0,0 +1,25 @@
+import os
+
+from random import randint
+
+
+PATH = './test_folder'
+TYPES = [
+    '.txt', '.csv', '.zip', '.exe', '.pickle', '.dat', '.bin', \
+    '.doc', '.docx', '.jpg', '.rar', '.zip', '.torrent', '.msi', \
+    '.java', '.html']
+
+
+def fill_folder(path):
+    """Filling 'PATH' folder with diffent TYPES of files"""
+    for i in range(len(TYPES)):
+        for k in range(1, randint(2, 10)):
+            with open(f'{PATH}/test_file{str(k)}{TYPES[i]}', 'wb') as f:
+                pass
+
+
+if __name__ == '__main__':
+    if not os.path.exists(PATH):
+        os.mkdir(PATH)
+
+    fill_folder(PATH)
---
+++ move_files_with_extension.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+
+
+PATH = './test_folder/'
+FILES = os.listdir(PATH)
+
+
+def create_folder(extension:str):
+    """Creating folder for each unique file extension"""
+    extension_path = PATH + f'{extension}/'
+    if extension not in os.listdir(PATH): # os.listdir(PATH) every time calculated
+        os.mkdir(extension_path)
+        # print(f'Create folder for .{extension}')
+    # print(f'Folder for .{extension} already exsists')
+    return extension_path
+
+
+def move_and_rename_file(file_name:str, extension:str):
+    """Moving file to folder and rename file if file exists in this folder"""
+    extension_path = create_folder(extension)
+    print(extension_path)
+    if file_name in os.listdir(extension_path):
+        index = 1
+        while file_name in os.listdir(extension_path):
+            new_file_name = file_name.replace(f'.{extension}', f'{index}.{extension}')
+
+            if new_file_name not in os.listdir(extension_path):
+                os.rename(PATH + file_name, PATH + new_file_name)
+                file_name = new_file_name
+            index += 1
+
+    shutil.move(PATH + file_name, extension_path + file_name)
+
+
+def main():
+    for f in FILES:
+        if os.path.isfile(PATH + f):
+            extension = f[f.rfind('.')+1:]
+            move_and_rename_file(f, extension)
+            # print(f'File {f} moved to "{extension}" folder')
+    print('All done')
+
+
+if __name__ == '__main__':
+    main()
